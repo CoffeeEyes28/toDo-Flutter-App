6e75cddf5ad87df0dae1d9a4ec3022579a444d0d
CoffeeEyes28 <liv@Olivias-MacBook-Air.local>
2023-08-27 18:06:58 -0400

    added color constants and updated app bar

---
+++ todo/lib/screens/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:todo/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpg'),
+            ),
+          )
+        ]),
+      ),
+      body: Container(
+        child: Text('This is the home screen'),
+      ),
+    );
+  }
+}
